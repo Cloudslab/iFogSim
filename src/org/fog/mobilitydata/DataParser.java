8338743daec6bd26a0df473f90274c5b8edbb613
saas <victoriabotelho14@gmail.com>
2021-12-02 16:12:29 -0300

    chore: removes unused dependency

--- src/org/fog/mobilitydata/DataParser.java
+++ src/org/fog/mobilitydata/DataParser.java
@@ -7,7 +7,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Random;
